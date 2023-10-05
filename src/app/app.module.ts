93bf3a7a9807e0384a600e7ff4376939a5eff3a2
Thiago Kachinsky <thiagokachidev@gmail.com>
2023-10-04 22:03:25 -0300

    Create project structure and UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { InputComponent } from './components/input/input.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    InputComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data.ts
@@ -0,0 +1,10 @@
+const data = {
+  "avatar_url": "https://avatars.githubusercontent.com/u/61670495?v=4",
+  "html_url": "https://github.com/ThiagoKachi",
+  "name": "Thiago Kachinsky",
+  "company": "Sinerlog",
+  "location": "Santa Catarina",
+  "email": null,
+  "public_repos": 49,
+  "created_at": "2020-03-02T00:40:12Z",
+}
